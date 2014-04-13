5f14596756ba5dd96062ec79847df1da6d16fbf6
Dhia Abbassi <dhiaabbassi90@gmail.com>
2014-04-13 21:24:16 +0200

    Rearranged folders

--- python/minesweeper_master/minesweeper_master.py
+++ python/minesweeper_master/minesweeper_master.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 
 """
-Unfortunatly this problem was not solved. It will be great if you can share your solution here.
+Unfortunatly this problem was not solved during the contest. It will be great if you can share your solution here.
 """
