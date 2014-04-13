605048ad7eb11dbd5e7fb69827c457383be7251f
Dhia Abbassi <dhiaabbassi90@gmail.com>
2014-04-13 10:30:34 +0200

    Added Deceitful War Solution.

---
+++ deceitful_war/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+__version__ = "0.1.0"
+__date__ = "2014-04-12 16:51:54"
+__author__ = "Dhia Abbassi"
+__email__ = "dhiaabbassi90@gmail.com"
+__license__ = "GPL"
+__maintainer__ = ""
+__doc__=""" Snippet of code that solves <Problem D: Deceitful War> """
